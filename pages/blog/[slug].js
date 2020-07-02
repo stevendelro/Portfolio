88051f27091e27bc4c3292c183fcfb307957d8ed
Steven Del Rosario <stevendelro@pm.me>
2020-07-02 11:40:27 -0700

    added components to PostDetail

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,11 +1,50 @@
-import { getStaticProps, getPostAndMorePosts, getAllPostsWithSlug } from '../../contentful-api/api'
-
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import ErrorPage from 'next/error'
+import Container from '@material-ui/core/Container'
+import Divider from '@material-ui/core/Divider'
+import {
+  getStaticProps,
+  getPostAndMorePosts,
+  getAllPostsWithSlug,
+} from '../../contentful/api'
+import PostTitle from '../../components/blog/PostTitle'
+import PostHeader from '../../components/blog/PostHeader'
+import PostBody from '../../components/blog/PostBody'
+import MoreStories from '../../components/blog/MoreStories'
 
 export default function PostDetail({ post, morePosts, preview }) {
+  const router = useRouter()
+  if (!router.isFallback && !post) {
+    return <ErrorPage statusCode={404} />
+  }
   return (
-    <div>
-      PostDetail.js
-    </div>
+    <Container>
+      <h1>PostDetail.js</h1>
+      {router.isFallback ? (
+        <PostTitle>Loading…</PostTitle>
+      ) : (
+        <>
+          <article>
+            <Head>
+              <title>{post.title} | Steven's Blog</title>
+              <meta property='og:image' content={post.coverImage.url} />
+            </Head>
+            <PostHeader
+              title={post.title}
+              coverImage={post.coverImage}
+              date={post.date}
+              author={post.author}
+            />
+            <PostBody content={post.content} />
+          </article>
+          <Divider />
+          {morePosts && morePosts.length > 0 && (
+            <MoreStories posts={morePosts} />
+          )}
+        </>
+      )}
+    </Container>
   )
 }
 
@@ -27,4 +66,3 @@ export async function getStaticPaths() {
     fallback: true,
   }
 }
-
