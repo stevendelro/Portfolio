aa8bc6712fa84f1736a2243fbc8c99c8dd7ce394
Steven Del Rosario <stevendelro@pm.me>
2020-07-03 15:05:01 -0700

    styled post preview

--- components/blog/PostPreview.js
+++ components/blog/PostPreview.js
@@ -1,18 +1,39 @@
 import Link from 'next/link'
 import { makeStyles } from '@material-ui/core/styles'
-import Avatar from '@material-ui/core/Avatar'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Divider from '@material-ui/core/Divider'
 import CardMedia from '@material-ui/core/CardMedia'
-import CustomDate from './CustomDate'
+import TitleBox from './shared/TitleBox'
 
 const useStyles = makeStyles(theme => ({
-  largeAvatar: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
+  postContent: {
+    marginBottom: theme.spacing(3),
   },
-  media: {
-    height: 200,
+
+  image: {
+    cursor: 'pointer',
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '3rem',
+    },
+    [theme.breakpoints.up('sm')]: {
+      borderRadius: 5,
+    },
+    [theme.breakpoints.down('lg')]: {
+      height: 200,
+    },
+
+    [theme.breakpoints.up('lg')]: {
+      height: 400,
+    },
+  },
+  textArea: {
+    padding: '0 1rem',
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '3rem',
+    },
   },
 }))
 
@@ -25,43 +46,50 @@ export default function PostPreview({
   slug,
 }) {
   const classes = useStyles()
-  const image = (
-    <CardMedia className={classes.media} title={title} image={coverImage} />
-  )
   return (
-    <Paper elevation={0}>
-      <div>
-        {slug ? (
+    <>
+      <Grid
+        container
+        className={classes.postContent}
+        direction='row'
+        justify='space-between'
+        alignItems='flex-start'>
+        <Grid item xs={12} sm={5}>
           <Link as={`/blog/${slug}`} href='/blog/[slug]'>
-            <a aria-label={title}>{image}</a>
+            <Paper elevation={3}>
+              <CardMedia
+                className={classes.image}
+                image={coverImage}
+                title={title}
+              />
+            </Paper>
           </Link>
-        ) : (
-          image
-        )}
-      </div>
-      <Typography variant='h4' gutterBottom>
-        <Link as={`/blog/${slug}`} href='/blog/[slug]'>
-          <a>{title}</a>
-        </Link>
-      </Typography>
-      <div>
-        <CustomDate dateString={date} />
-      </div>
-      <Typography variant='subtitle1' gutterBottom>
-        {excerpt}
-      </Typography>
-      {author && (
-        <>
-          <Avatar
-            className={classes.largeAvatar}
-            alt={author.name}
-            src={author.picture.url}
-          />
-          <Typography variant='h4' gutterBottom>
-            {author.name}
-          </Typography>
-        </>
-      )}
-    </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={7}>
+          <Grid
+            container
+            className={classes.textArea}
+            direction='column'
+            justify='space-around'
+            alignItems='flex-start'>
+              <TitleBox
+                title={title}
+                author={author}
+                date={date}
+                slug={slug}
+                variant='h4'
+              />
+            <Grid item>
+              <Typography variant='body1' gutterBottom>
+                {excerpt}
+              </Typography>
+            </Grid>
+          </Grid>
+              <Divider style={{ margin: '2rem 6rem'}}/>
+        </Grid>
+
+      </Grid>
+    </>
   )
 }
