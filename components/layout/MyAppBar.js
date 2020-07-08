22abf6172cd3e04c7a88af4736e998787fb6b592
Steven Del Rosario <stevendelro@pm.me>
2020-07-08 15:54:11 -0700

    Tabs now in appbar. display crumbs on scroll

--- components/layout/MyAppBar.js
+++ components/layout/MyAppBar.js
@@ -1,16 +1,20 @@
 import { useState, cloneElement } from 'react'
+import Router from 'next/router'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import Slide from '@material-ui/core/Slide'
 import Box from '@material-ui/core/Box'
 import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
+import Grow from '@material-ui/core/Grow'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
 import Switch from '@material-ui/core/Switch'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormGroup from '@material-ui/core/FormGroup'
 import Hidden from '@material-ui/core/Hidden'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 import NavCrumbs from './NavCrumbs'
+import HomeMenu from './HomeMenu'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,6 +22,9 @@ const useStyles = makeStyles(theme => ({
   },
   appBar: {
     backgroundColor: theme.palette.grey[100],
+  },
+  toolBar: {
+    minHeight: 48,
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -33,20 +40,43 @@ const useStyles = makeStyles(theme => ({
 }))
 
 // AppBar will cast a box shadow when content scrolls under it
-function ElevationScroll(props) {
-  const { children, window } = props
+function ElevationScroll({ children }) {
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
-    target: window ? window() : undefined,
   })
   return cloneElement(children, {
     elevation: trigger ? 4 : 0,
   })
 }
 
+function HideOnScroll({ children }) {
+  const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 100 })
+  const toggleDisplay = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 110,
+  })
+  return (
+    <Slide
+      appear={true}
+      direction='down'
+      in={!trigger}
+      display={toggleDisplay && 'none'}>
+      {children}
+    </Slide>
+  )
+}
+function ShowOnScroll({ children }) {
+  const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 125 })
+  return (
+    <Grow appear={false} direction='right' in={trigger}>
+      {children}
+    </Grow>
+  )
+}
+
 export default function MenuAppBar(props) {
-  const { toggleShowDrawer, darkMode, setDarkMode } = props
+  const { darkMode, setDarkMode } = props
   const classes = useStyles()
 
   const toggleThemeColorChange = event => {
@@ -57,22 +87,17 @@ export default function MenuAppBar(props) {
     <div className={classes.root}>
       <ElevationScroll {...props}>
         <AppBar className={classes.appBar} position='fixed'>
-          <Toolbar>
-            <Hidden xsDown>
-              <IconButton
-                edge='start'
-                className={classes.menuButton}
-                onClick={toggleShowDrawer()}
-                color='inherit'
-                aria-haspopup='true'
-                aria-label='main-menu'>
-                <MenuIcon />
-              </IconButton>
-            </Hidden>
-
-            <Box className={classes.crumbs}>
-              <NavCrumbs />
-            </Box>
+          <Toolbar className={classes.toolBar}>
+            <HideOnScroll {...props}>
+              <Box className={classes.crumbs}>
+                <HomeMenu />
+              </Box>
+            </HideOnScroll>
+            <ShowOnScroll {...props}>
+              <Box className={classes.crumbs}>
+                <NavCrumbs />
+              </Box>
+            </ShowOnScroll>
             <Hidden xsDown>
               <FormGroup>
                 <FormControlLabel
