9ff54caf5b066d674158c80c04ce8405b68e7805
Steven Del Rosario <stevendelro@pm.me>
2020-07-13 21:19:12 -0700

    now flexbox responsive across all widths

--- components/blog/PostPreviews/MainBlogList.js
+++ components/blog/PostPreviews/MainBlogList.js
@@ -1,32 +1,36 @@
-import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import HorizontalPreview from './HorizontalPreview'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
-  blogIndexList__Container: {
-
+  blogIndexList__Section: {
     backgroundColor:
       theme.palette.type === 'dark'
         ? theme.palette.common.defaultDarkBackground
         : theme.palette.common.defaultLightBackground,
   },
-  blogIndexList__GridContainer: {
-    // maxWidth: 600,
+  blogIndexList__Container: {
+    [theme.breakpoints.down('xs')]: {
+      padding: theme.spacing(0, 1)
+    }
   },
+  blogIndexList__GridItem: {
+    flexGrow: 1
+  }
 }))
 
 export default function MainBlogList({ posts }) {
   const classes = useStyles()
   return (
-    <section id='MainBlogList'>
-      <Box className={classes.blogIndexList__Container}>
-        <Grid container className={classes.blogIndexList__GridContainer} >
+    <section className={classes.blogIndexList__Section} id='MainBlogList'>
+      <Container className={classes.blogIndexList__Container} maxWidth='md'>
+        <Grid container  >
           {posts.map(post => {
             return (
-              <Grid item>
+              <Grid item className={classes.blogIndexList__GridItem}>
                 <HorizontalPreview
-                  key={post.slug}
+                  key={post.id}
                   title={post.title}
                   coverImage={post.coverImage.url}
                   date={post.date}
@@ -38,7 +42,7 @@ export default function MainBlogList({ posts }) {
             )
           })}
         </Grid>
-      </Box>
+      </Container>
     </section>
   )
 }
