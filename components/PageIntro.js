defe70f91b7ada61ce8b5cd983e70e605deb2372
Steven Del Rosario <stevendelro@pm.me>
2020-07-14 10:32:03 -0700

    complete refactor to simplify and fine-tune styles

--- components/PageIntro.js
+++ components/PageIntro.js
@@ -5,84 +5,83 @@ import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
-  smallScreen: {
-    padding: theme.spacing(8, 0, 6),
+  PageIntro__root: {
     backgroundColor:
-      theme.palette.type === 'dark'
-        ? theme.palette.common.defaultDarkBackground
-        : theme.palette.common.defaultLightBackground,
-    [theme.breakpoints.up('md')]: {
-      marginBottom: '3rem',
+    theme.palette.type === 'dark'
+      ? theme.palette.common.defaultDarkBackground
+      : theme.palette.common.defaultLightBackground,
+  },
+  PageIntro__Container: {
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+  PageIntro__title: {
+    width: 'fit-content',
+    padding: theme.spacing(2),
+    [theme.breakpoints.only('sm')]: {
+      padding: theme.spacing(2, 0, 0, 0),
+    },
+    [theme.breakpoints.down('xs')]: {
+      padding: theme.spacing(10, 0, 5, 0),
+
     },
   },
-  notSmallScreen: {
-    padding: theme.spacing(5, 0, 5, 0),
-    backgroundColor:
-      theme.palette.type === 'dark'
-        ? theme.palette.common.defaultDarkBackground
-        : theme.palette.common.defaultLightBackground,
-  },
-  paragraph: {
-    [theme.breakpoints.down('sm')]: {
-      margin: theme.spacing(0, 1, 0, 1),
-    },
-    margin: theme.spacing(0, 5, 0, 5),
+  PageIntro__paragraph: {
     fontSize: '1.3rem',
+    padding: theme.spacing(),
+
+    [theme.breakpoints.between('lg','xl')]: {
+      padding: theme.spacing(0, 7),
+      marginBottom: 0
+    },
+    [theme.breakpoints.only('md')]: {
+      padding: theme.spacing(0, 7),
+      marginBottom: 0
+    },
+    [theme.breakpoints.only('sm')]: {
+      paddingBottom: theme.spacing(2),
+    },
+    [theme.breakpoints.down('xs')]: {
+      padding: theme.spacing(0, 1, 6),
+    },
   },
 }))
 
 export default function PageIntro({ title, paragraph }) {
   const classes = useStyles()
   const theme = useTheme()
-  const xSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
-  if (xSmallScreen) {
-    return (
-      <header className={classes.smallScreen}>
-        <Container maxWidth='sm'>
-          <Typography component='h1' variant='h1' align='center' gutterBottom>
-            {title}
-          </Typography>
-          <Typography
-            className={classes.paragraph}
-            variant='body2'
-            align='center'
-            color='textSecondary'
-            paragraph>
-            {paragraph}
-          </Typography>
-        </Container>
-      </header>
-    )
-  }
-
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
   return (
-    <header className={classes.notSmallScreen}>
-      <Grid container direction='row' justify='center' alignItems='center'>
-        <Grid item xs={12} sm={10} lg={8} xl={6}>
-          <Grid container direction='row' justify='center' alignItems='center'>
-            <Grid item sm={3}>
-              <Typography
-                component='h1'
-                variant='h1'
-                align='center'
-                gutterBottom>
-                {title}
-              </Typography>
-            </Grid>
-
-            <Grid item sm={9}>
-              <Typography
-                className={classes.paragraph}
-                variant='body2'
-                align='center'
-                color='textSecondary'
-                paragraph>
-                {paragraph}
-              </Typography>
-            </Grid>
+    <header className={classes.PageIntro__root}>
+      <Container maxWidth='md' className={classes.PageIntro__Container}>
+        <Grid
+          container
+          direction={isSmallScreen ? 'column' : 'row'}
+          justify='center'
+          alignItems='center'>
+          <Grid item xs={12} sm={4} md={3}>
+            <Typography
+              className={classes.PageIntro__title}
+              component='h1'
+              variant='h1'
+              align='center'
+              gutterBottom>
+              {title}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={8} md={9}>
+            <Typography
+              className={classes.PageIntro__paragraph}
+              component='p'
+              variant='body2'
+              color='textSecondary'
+              align='center'
+              paragraph>
+              {paragraph}
+            </Typography>
           </Grid>
         </Grid>
-      </Grid>
+      </Container>
     </header>
   )
 }
