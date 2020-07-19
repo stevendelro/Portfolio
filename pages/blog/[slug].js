7a08b38913c9d3f4e04addc4ceb75ba00189438a
Steven Del Rosario <stevendelro@pm.me>
2020-07-19 11:12:35 -0700

    fix: medium screen widths now handled

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -85,7 +85,7 @@ export default function SlugPage({ post, morePosts, preview }) {
               direction='column'
               justify='center'
               alignItems='center'>
-              <Grid item className={classes.body} xs={12} sm={10} lg={6} xl={4}>
+              <Grid item className={classes.body} xs={12} sm={10} md={8} lg={6} xl={4}>
                 <PostBody content={post.content} />
               </Grid>
             </Grid>
