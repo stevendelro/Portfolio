0b63c8df28499a2f8dc0c6fbb5d207fd4ad918f0
Steven Del Rosario <stevendelro@pm.me>
2020-07-26 17:05:04 -0700

    add all components to ProjectDetails page

--- pages/work/[...projectDetails].js
+++ pages/work/[...projectDetails].js
@@ -4,12 +4,15 @@ import Grid from '@material-ui/core/Grid'
 import Head from 'next/head'
 
 import { getRepositoryNamesForPaths, getProjectDetails } from '../api/github'
+import { weathernautInfo } from './index'
 import Body from '../../components/work/ProjectDetailsPage/Body'
+import Description from '../../components/work/ProjectDetailsPage/Description'
+import GithubStamp from '../../components/work/ProjectDetailsPage/GithubStamp'
 import Header from '../../components/work/ProjectDetailsPage/Header'
 import Image from '../../components/work/ProjectDetailsPage/Image'
 
 const useStyles = makeStyles(theme => ({
-  rootProjectDetailsPage: {
+  rootArticle_ProjectDetails: {
     backgroundColor:
       theme.palette.type === 'dark'
         ? theme.palette.common.defaultDarkBackground
@@ -24,21 +27,40 @@ const useStyles = makeStyles(theme => ({
       padding: theme.spacing(3, 3, 0, 3),
     },
   },
-  body: {
-    padding: theme.spacing(3),
-  },
 }))
 
-export default function ProjectDetailsPage({ markdown, repo, user }) {
+export default function ProjectDetailsPage({ markdown, repo }) {
   const classes = useStyles()
-  console.log('getStaticProps: markdown', markdown)
-  console.log('getStaticProps: repo', repo)
-  console.log('getStaticProps: user', user)
   return (
-    <>
-      <h1>placeholder</h1>
-      
-    </>
+    <article className={classes.rootArticle_ProjectDetails}>
+      <Grid container direction='column' justify='center' alignItems='center'>
+        <Grid className={classes.header} item xs={12} sm={8} lg={6} xl={4}>
+          <Header
+            projectName={repo.name}
+            dateCreated={repo.dateCreated}
+            lastUpdated={repo.lastUpdated}
+          />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        direction='column'
+        justify='space-between'
+        alignItems='center'>
+        <Grid item xs={12} sm={11}>
+          <Container maxWidth='lg'>
+            <Image
+              title={repo.name}
+              imagePath={weathernautInfo.imagePath}
+              website={repo.website}
+            />
+            <Description description={repo.description} />
+            <Body content={markdown} />
+            <GithubStamp liveDemo={repo.website} sourceCode={repo.github} />
+          </Container>
+        </Grid>
+      </Grid>
+    </article>
   )
 }
 export async function getStaticProps({ params }) {
