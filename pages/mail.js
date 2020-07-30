43c4d8f08d07a2c99b50bea043b9f04d5fc1f05d
Steven Del Rosario <stevendelro@pm.me>
2020-07-30 16:32:48 -0700

    added vector art to /mail

--- pages/mail.js
+++ pages/mail.js
@@ -1,31 +1,49 @@
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 
+import { MailDark, MailLight } from '../components/svg/Mail'
 import PageIntro from '../components/PageIntro'
 
 const useStyles = makeStyles(theme => ({
   rootMailPage: {
-    minHeight: '100vh',
+    minHeight: '90vh',
     backgroundColor:
       theme.palette.type === 'dark'
         ? theme.palette.common.defaultDarkBackground
         : theme.palette.common.defaultLightBackground,
+    [theme.breakpoints.down('sm')]: {
+      padding: theme.spacing(0, 2),
+    },
   },
   paper: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
   },
-
+  image: {
+    [theme.breakpoints.up('md')]: {
+      paddingRight: theme.spacing(5),
+    },
+    [theme.breakpoints.only('sm')]: {
+      padding: theme.spacing(0, 1),
+      marginBottom: theme.spacing(7),
+    },
+    [theme.breakpoints.down('xs')]: {
+      padding: theme.spacing(0, 1),
+      marginBottom: theme.spacing(7),
+    },
+  },
   form: {
     width: '100%', // Fix IE 11 issue.
+    maxWidth: 500,
     marginTop: theme.spacing(3),
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
   submit: {
     color:
@@ -65,6 +83,9 @@ const validationSchema = Yup.object({
 })
 export default function Mail() {
   const classes = useStyles()
+  const theme = useTheme()
+  const type = theme.palette.type
+  const isDarkMode = type === 'dark' ? true : false
   const formik = useFormik({
     initialValues,
     onSubmit,
@@ -82,82 +103,90 @@ export default function Mail() {
   return (
     <article id='MainMailPageBody' className={classes.rootMailPage}>
       <PageIntro title='Mail' paragraph={heroParagraph} />
-      <Container component='main' maxWidth='xs'>
-        <CssBaseline />
-        <div className={classes.paper}>
-          <form className={classes.form} onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoFocus
-                  fullWidth
-                  id='firstName'
-                  name='firstName'
-                  label='First Name'
-                  autoComplete='given-name'
-                  variant='outlined'
-                  color='secondary'
-                  {...getFieldProps('firstName')}
-                  error={errors.firstName && Boolean(touched.firstName)}
-                  helperText={touched.firstName ? errors.firstName : ''}
-                />
+      <Container maxWidth='lg'>
+        <Grid
+          container
+          direction='row'
+          justify='space-around'
+          alignItems='center'>
+          <Grid item className={classes.image} xs={12} md={8}>
+            {isDarkMode ? <MailDark /> : <MailLight />}
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <form className={classes.form} onSubmit={handleSubmit}>
+              <Grid container spacing={1}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoFocus
+                    fullWidth
+                    id='firstName'
+                    name='firstName'
+                    label='First Name'
+                    autoComplete='given-name'
+                    variant='outlined'
+                    color='secondary'
+                    {...getFieldProps('firstName')}
+                    error={errors.firstName && Boolean(touched.firstName)}
+                    helperText={touched.firstName ? errors.firstName : ''}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    id='lastName'
+                    name='lastName'
+                    label='Last Name'
+                    autoComplete='family-name'
+                    variant='outlined'
+                    color='secondary'
+                    {...getFieldProps('lastName')}
+                    error={errors.lastName && Boolean(touched.lastName)}
+                    helperText={touched.lastName ? errors.lastName : ''}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    id='email'
+                    name='email'
+                    label='Email'
+                    autoComplete='email'
+                    variant='outlined'
+                    color='secondary'
+                    {...getFieldProps('email')}
+                    error={errors.email && Boolean(touched.email)}
+                    helperText={touched.email ? errors.email : ''}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    multiline
+                    rows={5}
+                    fullWidth
+                    id='message'
+                    name='message'
+                    type='message'
+                    label='Message'
+                    autoComplete='message'
+                    variant='outlined'
+                    color='secondary'
+                    onChange={handleChange}
+                    value={values.message}
+                    placeholder='Feel free to include any details.'
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  id='lastName'
-                  name='lastName'
-                  label='Last Name'
-                  autoComplete='family-name'
-                  variant='outlined'
-                  color='secondary'
-                  {...getFieldProps('lastName')}
-                  error={errors.lastName && Boolean(touched.lastName)}
-                  helperText={touched.lastName ? errors.lastName : ''}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  id='email'
-                  name='email'
-                  label='Email'
-                  autoComplete='email'
-                  variant='outlined'
-                  color='secondary'
-                  {...getFieldProps('email')}
-                  error={errors.email && Boolean(touched.email)}
-                  helperText={touched.email ? errors.email : ''}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  multiline
-                  rows={5}
-                  fullWidth
-                  id='message'
-                  name='message'
-                  type='message'
-                  label='Message'
-                  autoComplete='message'
-                  variant='outlined'
-                  color='secondary'
-                  onChange={handleChange}
-                  value={values.message}
-                  placeholder='Feel free to include any details.'
-                />
-              </Grid>
-            </Grid>
-            <Button
-              fullWidth
-              type='submit'
-              variant='contained'
-              color='primary'
-              className={classes.submit}>
-              Send
-            </Button>
-          </form>
-        </div>
+              <Button
+                fullWidth
+                type='submit'
+                variant='contained'
+                color='primary'
+                className={classes.submit}>
+                Send
+              </Button>
+            </form>
+          </Grid>
+        </Grid>
       </Container>
     </article>
   )
