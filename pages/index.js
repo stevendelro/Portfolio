b632b676dbec6784fe0eb4b7da1dee44c0806815
Steven Del Rosario <stevendelro@pm.me>
2020-08-01 11:19:58 -0700

    rewriten home page paragraphs

--- pages/index.js
+++ pages/index.js
@@ -29,44 +29,50 @@ const useStyles = makeStyles(theme => ({
 const heroParagraph = `
 You just found the front page of my life. Chances are, if you're reading
 this—it may already be too late. Or maybe not. Maybe this is the right
-time and it was all meant to be. Regardless, I made this all for you.
+time and it was all meant to be. Regardless, I made this for you.
 `
 const aboutText = `
-I’m Steven. I’m the guy who designed and developed this site. Im pretty
-proud of the Work page. Click on a project and you’ll see that I’m rendering
-the markdown from the same README.md file that’s posted on that project’s
-Github. Also, I’ve got my blog hooked up to parse content from Contentful.
-Fun stuff. Down below, you’ll find a little more info on who I am. Enjoy.
+I’m Steven. I designed and developed this site. Here’s a quick rundown:
+the two big pages in this portfolio are Work and Blog. I customized the
+markdown renderer that Material UI uses for its official docs in order to
+render the markdown from each project’s readme. Blog uses Contentful as a
+headless CMS and its design was heavily influenced by Ghost and Medium.
+All vector art was created from Katerina Limpitsouni, over at undraw.co.
 `
 const partyText = `
-I’m a people person. Whether it’s on the phone, through text/email, or
-in-person—I’m fascinated with trying to understand people. Some of the
-best work that I’ve done has come from working within a relatively small
-teams. If the vibe is right and the support is there, the possibility for
-the group to enter a state of "flow” is a huge motivator for me.
+I’m a people person. You could put me in a multitude of situations, with
+nearly any kind of personality type, and I could develop banter, establish
+rapport, and build strong connections with strangers with little to no
+effort. Some of the best work that I’ve done has come from working within
+a relatively small teams. If the vibe is right and everyone is driven, the
+possibility for the group to enter a state of "flow” is a huge motivator
+for me.
 `
 const exploreText =`
 I always find a way to balance out the time I spend sitting at a desk
 with doing something active. Back before all the travel restrictions, my
 girlfriend and I would routinely make plans to visit different cities and
 check out all the national parks. Some couples don’t travel well, but her
-and I love it. Give us a couple funny podcasts, some good books, a long
+and I love it. Give us a couple funny podcasts, some audiobooks, a long
 road—and we’re good to go.
 `
 const hangoutText =`
-I’m an inquisitive guy. If something piques my interest, my curiosity has
-a way to really feed itself. When it comes to having discussions, I’m
-definitely someone who strives for depth, rather than just skimming surface
-layer topics. This has led me to have some of the most interesting
-conversations. This world is built on the stories we share.
+I’m fairly inquisitive. If something piques my interest, it doesn’t take
+much to allow my curiosity to take over. In conversation, I enjoy quick
+exchanges filled with clever quips and references as much as anyone. But
+I really prefer going in-depth. If the world we live in was built on the
+stories we share, you could find me at the forefront of nuance, chipping
+away at the ineffable.
 `
+
 const workText =`
-And here I am now. I’ve spent the past ten or so years of my life working
-some interesting jobs, meeting a ton of people and having some great
-conversations. Two years ago, I got into web development and I made a couple
-apps. Now, with the pandemic in full swing, I’ve been making the most of it.
-The hours seem to literally melt away as I solve problem after problem. It's
-unlike anything I've ever done.
+And here I am now. With the pandemic in full swing, I’ve been making the
+most of it. Each day, the hours seem to literally melt away as I solve
+problem after problem. Web development is unlike anything I've ever done.
+I’m glad you’re able to see this portfolio. It’s been a labor of love, but
+if it’s anything like writing, I’ll look back on this in a few months and
+cringe. Anyway, head over to Mail to find my contact info. Feel free to
+hit me up. Enjoy.
 `
 
 const defaultParagraph = `
