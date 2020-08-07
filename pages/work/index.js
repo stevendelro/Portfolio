8a182427d97173c1cb68a9012d566fa4c51d6b74
Steven Del Rosario <stevendelro@pm.me>
2020-08-06 18:32:47 -0700

    replaced webp image with png

--- pages/work/index.js
+++ pages/work/index.js
@@ -63,7 +63,7 @@ up to date, while motivating me to make better writeups.
 
 export const weathernautInfo = {
   name: 'Weathernaut',
-  imagePath: '/weathernaut.webp',
+  imagePath: '/weathernaut.png',
   website: 'https://weathernaut.now.sh',
   github: 'https://github.com/stevendelro/Weathernaut',
   gitUsername: 'stevendelro',
